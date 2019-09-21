52b4d2b7c7d4923b08a647a142fa447f529de21d
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-09-20 23:05:07 -0300

    Aula 05 HttpClient POST. Validators

--- src/app/modules/cadastro/cadastro.component.ts
+++ src/app/modules/cadastro/cadastro.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { UserInputDTO } from 'src/app/models/dto/user-input';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'cmail-cadastro',
@@ -8,21 +11,86 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class CadastroComponent implements OnInit {
 
-  formCadastro = new FormGroup({
-    nome: new FormControl(),
-    username: new FormControl(),
-    senha: new FormControl(),
-    avatar: new FormControl()
-  })
 
-  constructor() { }
+  private validadoresNome = Validators.compose([
+    Validators.required,
+    Validators.minLength(2),
+    Validators.maxLength(255),
+    Validators.pattern('[a-zA-Z\u00C0-\u00FF ]+')
+  ]);
+
+  private validadoresUsername = Validators.compose([
+    Validators.required,
+    Validators.minLength(2),
+    Validators.maxLength(255),
+    Validators.pattern('[a-z0-9]+')
+  ]);
+
+  private validadoresSenha = Validators.compose([
+    Validators.required,
+    Validators.minLength(2),
+    Validators.maxLength(255)
+  ]);
+
+  private validadoresTelefone = Validators.compose([
+    Validators.required,
+    Validators.minLength(8),
+    Validators.maxLength(9),
+    Validators.pattern(/^\d{8,9}$/)
+  ]);
+
+  public nome = new FormControl('', this.validadoresNome);
+  public username = new FormControl('', this.validadoresUsername);
+  public senha = new FormControl('', this.validadoresSenha);
+  public avatar = new FormControl('', Validators.required);
+  public telefone = new FormControl('', this.validadoresTelefone);
+
+  public formCadastro = new FormGroup({
+    nome: this.nome,
+    username: this.username,
+    senha: this.senha,
+    avatar: this.avatar,
+    telefone: this.telefone,
+  });
+
+  public mensagem = '';
+
+  constructor(private conexaoApi: HttpClient
+              ,private roteador: Router) { }
 
   ngOnInit() {
+
+    //this.formCadastro.get('nome').getError('minlength')
   }
 
   handleCadastro() {
-    console.log(this.formCadastro.value);
 
+    if(this.formCadastro.invalid) {
+      console.log('INVALIDOOO');
+
+      return;
+    }
+
+    //data transfer object -DTO
+    const userDTO = new UserInputDTO(this.formCadastro.value);
+
+    this.conexaoApi
+        .post('http://localhost:3200/users', userDTO)
+        .subscribe(
+          resposta => {
+            console.log(resposta);
+            this.mensagem = "Cadastro feito com sucesso!"
+            this.formCadastro.reset();
+          }
+          ,(erro: HttpErrorResponse) => {
+            this.mensagem = erro.error.body[0].message;
+          }
+          ,() => {
+            setTimeout(() => {
+              this.roteador.navigate(['login']);
+            }, 3000);
+          }
+        );
   }
 
 }
--- src/app/modules/cadastro/cadastro.module.ts
+++ src/app/modules/cadastro/cadastro.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { CadastroComponent } from './cadastro.component';
 import { SharedComponentsModule } from 'src/app/components/shared-components.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [CadastroComponent],
@@ -10,7 +11,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     SharedComponentsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ]
 })
 export class CadastroModule { }
