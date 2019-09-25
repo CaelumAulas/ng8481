cb824f14adc1a250aab69780149e059b379f2c87
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-09-24 22:58:31 -0300

    Aula 06 LoginComponent

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { LoginOutput } from 'src/app/models/login-output';
 
 @Component({
   selector: 'cmail-login',
   templateUrl: './login.component.html',
-  styles: []
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public email = new FormControl('', Validators.required);
+  public senha = new FormControl('', Validators.required);
 
-  ngOnInit() {
+  public formLogin = new FormGroup({
+    email: this.email,
+    senha: this.senha
+  })
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {}
+
+  handleLogin(){
+
+    const loginDto = {
+      email: this.email.value,
+      password: this.senha.value
+    }
+    console.log(this.formLogin.value);
+
+    this.http
+        .post('http://localhost:3200/login', loginDto)
+        .subscribe(
+          (resposta: LoginOutput) => {
+            console.log('deu certo');
+            localStorage.setItem('cmail-token', resposta.token)
+          }
+          , (erro) => {
+            console.log('deu ruim');
+          }
+        )
+
   }
 
 }
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { RouterModule } from '@angular/router';
 import { LoginRoutingModule } from './login-routing.module';
+import { SharedComponentsModule } from 'src/app/components/shared-components.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -11,7 +14,10 @@ import { LoginRoutingModule } from './login-routing.module';
   imports: [
     CommonModule,
     RouterModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    SharedComponentsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   exports: [LoginComponent]
 })
